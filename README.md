8f52efa6e0efbe72264fd318447ea598f7f552e7
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-25 15:24:03 +0100

    Add source license and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# pre-commit-hooks
-A collection of pre-commit hooks
+# Useful Pre-commit Hooks
+A collection of useful pre-commit hooks. 
+
+## Usage
+
+We current support common hooks for C++ development. Here is the list: 
+
+```cpp-hooks
+cmake-format
+cmake-lint
+clang-format
+cpplint
+```
+
+To use these hooks, simply place the following in your `.pre-commit-config.yaml`
+
+For example: 
+```yaml
+repos:
+  - repo: https://github.com/xiaoyuechen/useful-pre-commit-hooks.git
+    rev: v0.7.5
+    hooks: 
+    - id: cmake-format
+      args: [--in-place, --line-ending=auto]
+    - id: cmake-lint
+      args: [--disabled-codes=C0327]
+    - id: clang-format
+    - id: cpplint
+      args: [--output=vs7]
+```
+
+Then run `pre-commit install`. Now every time when you run `git commit`, the selected 
+pre-commit commands are going to run. 
--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+# Copyright 2020 Xiaoyue Chen
+#
+# This file is part of "Useful Pre-commit Hooks" at
+#
+#     https://github.com/xiaoyuechen/useful-pre-commit-hooks.git
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import setuptools
 
 
@@ -5,7 +23,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pre-commit-hooks-xiaoyuechen", 
+    name="pre-commit-hooks-xiaoyuechen",
     version="0.0.1",
     author="Xiaoyue Chen",
     author_email="xiaoyue_chen@outlook.com",
@@ -14,7 +32,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/xiaoyuechen/pre-commit-hooks.git",
     packages=setuptools.find_packages(),
-    install_requires = ["cpplint", "cmakelang"],
     classifiers=[
         "Topic :: Software Development :: Version Control :: Git",
         "License :: OSI Approved :: Apache Software License",
